068df40c4c5d162f404b2216208807131f19c558
emrecodes <87083176+emrecodes@users.noreply.github.com>
2022-03-17 10:45:00 +0300

    Add files via upload

---
+++ Bash/expect/sftpbot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+HOST=hostAddress
+PORT=portNo
+USER=userName
+PASSWORD=password
+
+#Makes the extension of all xyz files in SourceFolder abc
+for x in /SourceFolder/*.xyz; do mv "$x" "${x%.xyz}.abc"; done
+
+SOURCE_FILE=/SourceFolder/*.abc
+TARGET_DIR=/TargetFolder/tmp
+
+#Use expect to automatically send all abc files to targer folder with sftp
+/usr/bin/expect<<EOD
+
+spawn /usr/bin/sftp -o Port=$PORT $USER@$HOST
+expect "password:"
+send "$PASSWORD\r"
+expect "sftp>"
+send "put $SOURCE_FILE $TARGET_DIR\r"
+expect "sftp>"
+send "bye\r"
+EOD
+
+
+###  Rename.sh => Add this script to crontab so files that have the extension changed to abc before sending will be xyz as in the original (Work at Sending Host)
+###  !/bin/bash
+###  for x in /SourceFolder/*.abc; do mv "$x" "${x%.abc}.xyz"; done
+
+###  Rename2.sh => Add this script to crontab so files that have sent as abc will display as their original extension xyz(Work at Receiving Host)
+###  #!/bin/bash
+###  for x in /TargetFolder/tmp/*.abc; do mv "$x" "${x%.abc}.xyz"; done
